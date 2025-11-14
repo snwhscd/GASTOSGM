650a6d1ac9ebc79c5d6d0d6bea8094123914ff8e
Snow-Hash-Code <gerardoluis206@gmail.com>
2025-11-14 01:50:22 -0600

    fixed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { PrismaClient } from "@/app/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
